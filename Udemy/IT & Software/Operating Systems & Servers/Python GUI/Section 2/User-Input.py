2c93305cdc990f3eebc510a8502962072120eb8d
Aภl๏miuภuຮ <quadcosmos@gmail.com>
2022-05-04 08:29:57 +0300

    Updates + Changes

---
+++ Udemy/IT & Software/Operating Systems & Servers/Python GUI/Section 2/User-Input.py
@@ -0,0 +1,19 @@
+import PySimpleGUIQt as sg
+
+form = sg.FlexForm('My first GUI')
+
+layout = [ [sg.Text('Enter your First Name', size=(15, 1)), sg.InputText()],
+           [sg.Text('Enter your Last Name', size=(15, 1)), sg.InputText()],
+           [sg.Text('Enter your country', size=(15, 1)), sg.InputText()],
+           [sg.Text('Enter your phone', size=(15, 1)), sg.InputText()],
+           [sg.Text('Enter your city', size=(15, 1)), sg.InputText()],
+           [sg.OK(), sg.Cancel()] ]
+
+button, values = form.Layout(layout).Read()
+name = values[0]
+lname = values[1]
+country = values[2]
+phone = values[3]
+city = values[4]
+
+print(f"First Name: {name}, Last Name: {lname}, Country: {country}, Phone: {phone}, City: {city}")
