180261fc69e1debd9c60a2453133033b7dd45359
Aภl๏miuภuຮ <quadcosmos@gmail.com>
2022-05-09 03:17:57 +0300

    Upload

--- Udemy/Development/Game Development/2D Game Development/Create snake with Python PyGame/5/collision.py
+++ Udemy/Development/Game Development/2D Game Development/Create snake with Python PyGame/5/collision.py
@@ -6,7 +6,7 @@ from random import randint
 # Player class
 class Player:
     x = 0
-    y = 0 
+    y = 0
     d = 0
     positions = []
     length = 4
@@ -47,40 +47,46 @@ class Game:
         self._display_surf = pygame.display.set_mode((640,480), pygame.HWSURFACE)
         pygame.display.set_caption('Pygame example')
 
-        # Load snake image
-        self._snake_image = pygame.image.load("snake.png").convert()
+        # Load lion image
+        self._lion_image = pygame.image.load("lion.png").convert()
 
         # Load apple image
         self._apple_image = pygame.image.load("apple.png").convert()
-       
-    def on_render(self):
-        self._display_surf.fill((0,0,0))
-        #self._display_surf.blit(self._snake_image, (self.player.x,self.player.y)) 
 
-        # Draw snake
+        # Load space image
+        self._space_image = pygame.image.load("space.jpeg").convert()
+
+    def on_render(self):
+        # Show space image
+        self._display_surf.blit(self._space_image, (0,0))
+
+        # self._display_surf.fill((0,0,0))
+        #self._display_surf.blit(self._lion_image, (self.player.x,self.player.y))
+
+        # Draw lion
         for pos in self.player.positions:
-            self._display_surf.blit(self._snake_image, (pos[0],pos[1])) 
+            self._display_surf.blit(self._lion_image, (pos[0],pos[1]))
 
         # Draw apple
         self._display_surf.blit(self._apple_image, (self.apple.x,self.apple.y))
 
         pygame.display.flip()
- 
+
     def on_cleanup(self):
         pygame.quit()
- 
+
     def on_execute(self):
         if self.on_init() == False:
             self._running = False
- 
+
         # Game loop
         while self._running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self._running = False
-                    
-            keys = pygame.key.get_pressed() 
-            
+
+            keys = pygame.key.get_pressed()
+
             if keys[K_RIGHT]:
                 self.player.d = 0
 
@@ -110,24 +116,25 @@ class Game:
 
             # Detect collision with apple
             if self.isCollision(self.player.x,self.player.y,self.apple.x,self.apple.y, 44):
-                print('collides')
                 self.apple.x = randint(0,self.game_width) * self.grid_size
                 self.apple.y = randint(0,self.game_height) * self.grid_size
                 self.player.length += 1
+                collides=int("1")
+                print(collides)
 
-            # Detect snake collection with itself
+            # Detect lion collection with itself
             if len(self.player.positions) > self.player.length-1:
-                for i in range(0,self.player.length-1):                    
+                for i in range(0,self.player.length-1):
                     if self.isCollision(self.player.x,self.player.y,self.player.positions[i][0], self.player.positions[i][1],40):
                         print('GAME OVER')
-                        exit()                      
-   
+                        exit()
+
             # Update screen
             self.on_render()
             time.sleep(0.1)
-  
+
         self.on_cleanup()
- 
+
 if __name__ == "__main__" :
     game = Game()
     game.on_execute()
